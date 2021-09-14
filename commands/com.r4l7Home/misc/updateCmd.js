004a0886705f53388a8d61f59d8de6b993aef91d
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-09-14 14:09:39 -0400

    Update updateCmd.js

--- commands/com.r4l7Home/misc/updateCmd.js
+++ commands/com.r4l7Home/misc/updateCmd.js
@@ -1,15 +1,9 @@
 module.exports = {
-
-	name: 'update',	code: `
-
+	name: 'update',
+	code: `
 	$updateCommands
-
 	$reply[$messageID;
-
-	<:warningHome:886302182293532722> **|** Comandos Carregados]
-
+	<:warningHome:886302182293532722>.  Comandos Carregados]
 	$onlyForIDs[$botOwnerID;]
-
 	`
-
 };
