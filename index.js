9e0f4ab4d7e82ea13f523d28c61b21118997e283
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-09-14 14:53:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,3 @@ bot.status({
 bot.variables({
 	prefix: "_"
 });
-
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
