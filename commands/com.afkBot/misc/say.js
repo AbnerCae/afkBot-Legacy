3d826c3ccb9e44400233156c22ca07f06cc17750
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-09-14 14:14:35 -0400

    Update say.js

--- commands/com.afkBot/misc/say.js
+++ commands/com.afkBot/misc/say.js
@@ -1,7 +1,6 @@
 module.exports = {
-	type: 'interactionCommand',
 	name: 'dizer',
 	code: `
-	$interactionReply[$message\n<:talkingHome:886288888795103253> **|** Mensagem enviada por <@$authorID>]
+	$reply[$messageID;$message\n<:talkingHome:886288888795103253> **|** Mensagem enviada por <@$authorID>]
 	`
 };
