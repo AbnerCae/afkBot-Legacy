071e553bef046826a5bdf7c42cde25331d86bc90
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-09-14 14:16:03 -0400

    Create musicStart.js

---
+++ commands/com.afkBot/events/music/musicStart.js
@@ -0,0 +1,17 @@
+module.exports = {
+	type: 'musicStartCommand',
+	channel: '$channelID',
+	code: `
+	$author[Tocando agora...;https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/twitter/282/musical-note_1f3b5.png]
+	$title[$songInfo[title];$songInfo[url]
+	$thumbnail[$songInfo[thumbnail]]
+	$addField[Duração:;$songInfo[duration_left];yes]
+	$footer[Adicionado por: $userTag[$songInfo[userID]];$userAvatar[$songInfo[userID]]]
+	$color[#83639D]
+	
+	$if[$isPrune==false]
+	$pruneMusic
+	$else
+	$endIf
+	`
+};
