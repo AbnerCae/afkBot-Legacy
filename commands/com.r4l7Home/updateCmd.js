45ea1afdbb998cdab0b27e87726837daa08c48af
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-09-14 13:54:33 -0400

    Create updateCmd.js

---
+++ commands/com.r4l7Home/updateCmd.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+	name: 'update',	code: `
+
+	$updateCommands
+
+	$reply[$messageID;
+
+	<:warningHome:886302182293532722> **|** Comandos Carregados]
+
+	$onlyForIDs[$botOwnerID;]
+
+	`
+
+};
