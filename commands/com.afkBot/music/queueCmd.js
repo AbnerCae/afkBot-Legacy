132f3fcb9eff67c41862b19eec40508801e36d5f
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-09-14 14:01:21 -0400

    Create queueCmd.js

---
+++ commands/com.afkBot/music/queueCmd.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+	name: 'queue-v2',	code: `
+
+	$reply[$messageID;> 🎶.  Tocando agora:\n> **$songInfo[title]**. Pedido pelo: <@$songInfo[userID]>.
+
+$if[$queue[1;3;{number} - {title}]!=]\n🗒️. Próximas músicas:\n$queue[1;7;  {number} - {title}. Pedido pelo <@{userID}>.]]$else$endIf
+
+	`
+
+};
