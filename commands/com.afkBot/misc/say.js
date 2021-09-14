bd83b633fa0f338ffbcf59d97e38491a86ad5de7
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-09-14 14:13:32 -0400

    Add files via upload

---
+++ commands/com.afkBot/misc/say.js
@@ -0,0 +1,7 @@
+module.exports = {
+	type: 'interactionCommand',
+	name: 'dizer',
+	code: `
+	$interactionReply[$message\n<:talkingHome:886288888795103253> **|** Mensagem enviada por <@$authorID>]
+	`
+};
