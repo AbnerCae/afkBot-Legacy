d325255a5132f2998b0274bcfe230a4a2cfcd7cd
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-09-21 22:42:55 -0400

    Update volumeCmd.js

--- commands/com.afkBot/music/volumeCmd.js
+++ commands/com.afkBot/music/volumeCmd.js
@@ -4,6 +4,7 @@ module.exports = {
 	code: `
 	$volume[$message[1]]
 	$reply[$messageID;🎶.  Volume setado para $message[1]]
+	$deleteIn[20s]
 	$onlyIf[$isNumber[$message[1]]!=false;:x:.  Use apenas numeros para defenir o Volume.]
 	`
 };
