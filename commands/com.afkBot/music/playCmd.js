cb75094747da307c7eb008534581e63605f29bf6
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-09-22 11:05:05 -0400

    Update playCmd.js

--- commands/com.afkBot/music/playCmd.js
+++ commands/com.afkBot/music/playCmd.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'play',
 	aliases: ['p', 'tocar'],
 	code: `
-	$reply[$messageID;🎶.  $playSong[$message;1m;yes;yes;:x:.  Erro ao reproduzir essa música.]]
-	$onlyIf[$message!=$reply[$messageID;:x:.  Diga o nome da música para ser tocada ou cole o link do vídeo.]]
+	$reply[$messageID;🎶  $playSong[$message;1m;yes;yes;:x:.  Erro ao reproduzir essa música.
+	$onlyIf[$message!=:x:.  Diga o nome da música para ser tocada ou cole o link do vídeo.]]
 	`
 };
