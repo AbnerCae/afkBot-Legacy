2991cdd7b80bc033e4d286066c7671f70dfd8def
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-09-21 22:44:39 -0400

    Update say.js

--- commands/com.afkBot/misc/say.js
+++ commands/com.afkBot/misc/say.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: 'dizer',
 	code: `
-	$reply[$messageID;$message\n<:talkingHome:886288888795103253> **|** Mensagem enviada por <@$authorID>]
+	$message\n<:talkingHome:886288888795103253> **|** Mensagem enviada por <@$authorID>
 	`
 };
